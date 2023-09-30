8f58a87922326cb3b848877fb36d0aeabb01c7f1
Eliel Moura <eliellpf@gmail.com>
2023-09-30 11:56:39 -0300

    loops

---
+++ js/07.loops.js
@@ -0,0 +1,33 @@
+/*  Principais comando de repetição
+
+- while      ENQUANTO
+- do/while   FAÇA/ENQUANTO
+- for        PARA
+
+OBS: normalmente os comando são controlados por meio de uma variável contadora.
+Nomes comuns para esta variável: i, j, k
+*/
+
+// exemplo 1: while
+/* let i = 1;
+while ( i <= 5) {
+    console.log(`Contador vale: ${i}`);
+    i++;
+} */
+
+// exemplo 2: do/while
+// este loop sempre funcionará pelo menos uma vez
+
+/* let i = 1;
+do {
+    alert("Oláaaa");
+    i++; // incremento
+} while (i <= 10); */
+
+// exemplo 3: loop for
+
+for (let i = 1; i <= 50; i++){
+console.log(`I vale ${i}`);
+}
+
+
