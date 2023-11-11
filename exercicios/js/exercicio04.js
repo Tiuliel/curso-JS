ee56dbaa76c53c539d993972236e87785a9f3323
Eliel Moura <eliellpf@gmail.com>
2023-11-11 08:42:43 -0300

    corrigindo o exercicio

--- exercicios/js/exercicio04.js
+++ exercicios/js/exercicio04.js
@@ -5,20 +5,20 @@ const cor2 = document.querySelector("#cor2")
 const cor3 = document.querySelector("#cor3")
 
 cor1.addEventListener("click", function(){
-    significado.innerHTML="<h3>A cor roxa está muitas vezes associado à espiritualidade, ao mistério e ao misticismo</h3>";
+    significado.innerHTML="A cor roxa está muitas vezes associado à espiritualidade, ao mistério e ao misticismo";
     significado.style.color="red"
     pagina.style.backgroundColor= "purple";
     pagina.style.transition="1s";
 })
 cor2.addEventListener("click", function(){
-    significado.innerHTML="<h3>Rosa é a cor do romantismo e da delicadeza.</h3>";
+    significado.innerHTML="Rosa é a cor do romantismo e da delicadeza.";
     significado.style.color= "purple"
-    pagina.style.backgroundColor="palevioletred";
+    pagina.style.backgroundColor="#ff41d0";
     pagina.style.transition="1s";
 })
 cor3.addEventListener("click", function(){
-    significado.innerHTML="<h3>Vermelho simboliza a paixão e o amor. </h3>";
-    significado.style.color="palevioletred"
+    significado.innerHTML="Vermelho simboliza a paixão e o amor.";
+    significado.style.color="#ff41d0"
     pagina.style.backgroundColor="red";
     pagina.style.transition="1s";
 })
