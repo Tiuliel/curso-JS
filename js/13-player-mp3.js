f952e52e3597b4ff8f2cd3a0b7e9cceed7ecd86f
Eliel Moura <eliellpf@gmail.com>
2023-11-25 09:27:53 -0300

    finalizando music player

--- js/13-player-mp3.js
+++ js/13-player-mp3.js
@@ -2,13 +2,27 @@
 
 const lista = document.querySelector("#lista-de-musicas");
 const reprodutor = document.querySelector("audio");
+const figureCapa = document.querySelector("figure");
+const imagemCapa = figureCapa.querySelector("img");
+const tituloMusica = figureCapa.querySelector("figcaption");
+
+
 
 /* Monitorando o evento de mudanças de opções na lista <select> */
 lista.addEventListener("change", function(){
+if (figureCapa.hasAttribute("hidden")) {
+    figureCapa.removeAttribute("hidden");
+}
+
     let musicaSelecionada = lista.options[lista.selectedIndex];
 console.log(musicaSelecionada);
 
 reprodutor.src = musicaSelecionada.value;
 
-reprodutor.play();
+tituloMusica.textContent = musicaSelecionada.textContent;
+
+imagemCapa.src = musicaSelecionada.getAttribute("data-imagem");
+
+if(musicaSelecionada.value !== "")
+{reprodutor.play();}
 })
